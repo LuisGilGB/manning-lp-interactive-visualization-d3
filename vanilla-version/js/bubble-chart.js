d147272db95b560e301ad1377ed5fa7536314a30
LuisGilGB <luis.gilgb@gmail.com>
2022-04-02 17:52:34 +0200

    Full working solution

--- vanilla-version/js/bubble-chart.js
+++ vanilla-version/js/bubble-chart.js
@@ -10,7 +10,8 @@
     const MAX_PADIUS = 30;
 
     const CHART_CONTAINER_SELECTOR = '#bubble-chart';
-    const LEGEND_CONTAINER_SELECTOR = '.legend-color'
+    const LEGEND_COLOR_CONTAINER_SELECTOR = '.legend-color';
+    const LEGEND_AREA_CONTAINER_SELECTOR = '.legend-area';
 
     const createBubbleChart = rawData => {
         const metrics = [
@@ -27,7 +28,8 @@
                 acc[metricKey] = parseFloat(d[metricKey]);
                 return acc;
             }, {
-                artist: d.artist
+                artist: d.artist,
+                title: d.title
             });
         });
 
@@ -94,14 +96,69 @@
             .attr('fill', d => colorScale(d.artist));
 
         const legendList = d3
-            .select(LEGEND_CONTAINER_SELECTOR)
-            .append('li');
+            .select(LEGEND_COLOR_CONTAINER_SELECTOR)
+            .append('ul');
 
         legendList
-            .selectAll('ul')
+            .selectAll('li')
             .data(data)
-            .join('ul')
-            .text(d => d.artist);
+            .join('li')
+            .attr('class', 'legend-list-item');
+
+        d3.selectAll('li.legend-list-item')
+            .append('span')
+            .attr('class', 'legend-circle')
+            .attr('style', d => `background-color: ${colorScale(d.artist)}`)
+
+        d3.selectAll('li.legend-list-item')
+            .append('span')
+            .text(d => `${d.title}, ${d.artist}`);
+
+        const LEGEND_BIG_CIRCLE_VALUE = 1.5;
+        const LEGEND_MEDIUM_CIRCLE_VALUE = 0.5;
+        const LEGEND_SMALL_CIRCLE_VALUE = 0.1;
+
+        const AREA_LEGEND_VALUES = [LEGEND_BIG_CIRCLE_VALUE, LEGEND_MEDIUM_CIRCLE_VALUE, LEGEND_SMALL_CIRCLE_VALUE];
+        const LEGEND_AREA_HEIGHT = 2 * bubblesAreaScale(LEGEND_BIG_CIRCLE_VALUE) + 10;
+
+        const legendArea = d3
+            .select(LEGEND_AREA_CONTAINER_SELECTOR)
+            .append('svg')
+            .attr('width', 300)
+            .attr('height', LEGEND_AREA_HEIGHT);
+
+        legendArea
+            .selectAll('g')
+            .data(AREA_LEGEND_VALUES)
+            .join('g')
+            .attr('class', 'legend-area-group')
+            .attr('transform', `translate(${bubblesAreaScale(LEGEND_BIG_CIRCLE_VALUE)},${LEGEND_AREA_HEIGHT})`)
+
+        d3.selectAll('g.legend-area-group')
+            .append('circle')
+            .attr('r', d => bubblesAreaScale(d))
+            .attr('cx', 0)
+            .attr('cy', d => -bubblesAreaScale(d))
+            .attr('fill', '#727a87')
+            .attr('opacity', 0.4);
+
+        const DASHED_LEGEND_LINE_LENGTH = 60;
+
+        d3.selectAll('g.legend-area-group')
+            .append('line')
+            .attr('x1', 0)
+            .attr('y1', d => -2 * bubblesAreaScale(d))
+            .attr('x2', DASHED_LEGEND_LINE_LENGTH)
+            .attr('y2', d => -2 * bubblesAreaScale(d))
+            .attr('stroke', '#333')
+            .attr('stroke-width', 1)
+            .attr('stroke-dasharray', 5);
+
+        d3.selectAll('g.legend-area-group')
+            .append('text')
+            .attr('x', DASHED_LEGEND_LINE_LENGTH + 5)
+            .attr('y', d => -2 * bubblesAreaScale(d) + 5)
+            .text(d => `${d}M`);
     }
 
     const data = await d3.csv('./data.csv');
