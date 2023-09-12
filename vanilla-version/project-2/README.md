e50877162abecf0685938b4edf4f14665c669e63
LuisGilGB <luis.gilgb@gmail.com>
2023-09-12 23:49:46 +0200

    Reorganized folders to separate projects and added initial files for the second project

---
+++ vanilla-version/project-2/README.md
@@ -0,0 +1,4 @@
+# Interactive Data Visualization with D3js - Play with Distributions
+## A Manning's liveProject
+
+These are the starter files for the liveProject Play with Distributions.
---
+++ vanilla-version/project-2/js/main.js
@@ -0,0 +1,18 @@
+const margin = {top: 45, right: 30, bottom: 50, left: 80};
+const width = 600; // Total width of the SVG parent
+const height = 600; // Total height of the SVG parent
+const padding = 1; // Vertical space between the bars of the histogram
+const barsColor = 'steelblue';
+
+// Load data here
+
+
+// Create Histogram
+const createHistogram = () => {
+  
+};
+
+// Create Split Violin Plot
+const createViolin = () => {
+  
+};
