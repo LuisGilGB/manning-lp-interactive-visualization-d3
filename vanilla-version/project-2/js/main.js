12dae41c194a4389ec891b061befa255ed16df1b
LuisGilGB <luis.gilgb@gmail.com>
2023-09-14 00:23:02 +0200

    Histogram progress

--- vanilla-version/project-2/js/main.js
+++ vanilla-version/project-2/js/main.js
@@ -7,7 +7,8 @@ const barsColor = 'steelblue';
 // Load data here
 d3.csv('data/pay_by_gender_tennis.csv').then(data => {
   console.log(data);
-  createHistogram(data);
+  const earnings = data.map(item => parseInt(item.earnings_USD_2019));
+  createHistogram(earnings);
 })
 .catch(error => {
   console.error(error);
@@ -15,8 +16,28 @@ d3.csv('data/pay_by_gender_tennis.csv').then(data => {
 
 
 // Create Histogram
-const createHistogram = () => {
-  
+const createHistogram = (earnings) => {
+  console.log(earnings);
+  const min = 0;
+  const max = Math.ceil(d3.max(earnings) / 1_000_000);
+  const bin = d3.bin().thresholds(max);
+  const bins = bin(earnings);
+  console.log(bins);
+
+  d3.select('#viz')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('class', 'histogram')
+    .selectAll('rect')
+    .data(bins)
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bins.map(bin => bin.length))])
+    .range([margin.left, width - margin.right])
+    .nice();
+  console.log(xScale.domain(), xScale.range());
 };
 
 // Create Split Violin Plot
