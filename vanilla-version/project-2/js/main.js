99371b591cf503791dd5022506fb8af8cbcbf921
LuisGilGB <luis.gilgb@gmail.com>
2023-09-15 23:31:11 +0200

    Added curve over the histogram

--- vanilla-version/project-2/js/main.js
+++ vanilla-version/project-2/js/main.js
@@ -1,4 +1,4 @@
-const margin = {top: 45, right: 30, bottom: 50, left: 80};
+const margin = { top: 45, right: 30, bottom: 50, left: 80 };
 const width = 600; // Total width of the SVG parent
 const height = 600; // Total height of the SVG parent
 const barsGap = 1; // Vertical space between the bars of the histogram
@@ -10,37 +10,37 @@ d3.csv('data/pay_by_gender_tennis.csv').then(data => {
   const earnings = data.map(item => parseInt(item.earnings_USD_2019));
   createHistogram(earnings);
 })
-.catch(error => {
-  console.error(error);
-});
+  .catch(error => {
+    console.error(error);
+  });
 
 
 // Create Histogram
 const createHistogram = (earnings) => {
-  console.log(earnings);
   const min = 0;
   const max = Math.ceil(d3.max(earnings) / 1_000_000);
   const bin = d3.bin().thresholds(max);
   const bins = bin(earnings);
-  console.log(bins);
 
   d3.select('#viz')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .attr('viewBox', `0 0 ${width} ${height}`)
-    .attr('class', 'histogram')
+    .attr('class', 'histogram');
 
   const xScale = d3.scaleLinear()
-    .domain([0, d3.max(bins.map(bin => bin.length))])
-    .range([margin.left, width - margin.right])
+    .domain([ 0, d3.max(bins.map(bin => bin.length)) ])
+    .range([ margin.left, width - margin.right ])
     .nice();
-  console.log(xScale.domain(), xScale.range());
-  const yScale = d3.scaleLinear()
-    .domain([0, bins.at(-1).x1])
-    .range([height - margin.bottom, margin.top]);
 
-  d3.select('.histogram')
+  const yScale = d3.scaleLinear()
+    .domain([ 0, bins.at(-1).x1 ])
+    .range([ height - margin.bottom, margin.top ]);
+
+  const histogram = d3.select('.histogram');
+
+  histogram
     .append('text')
     .attr('x', width / 2)
     .attr('y', margin.top / 2)
@@ -48,21 +48,21 @@ const createHistogram = (earnings) => {
     .attr('class', 'title')
     .text('Earnings of the top tennis players in 2019 (USD)');
 
-  d3.select('.histogram')
+  histogram
     .append('g')
     .attr('transform', `translate(${margin.left}, 0)`)
     .call(d3
       .axisLeft(yScale)
       .ticks(bins.length)
-      .tickFormat(d3.format('.2s'))
+      .tickFormat(d3.format('.2s')),
     );
 
-  d3.select('.histogram')
+  histogram
     .append('g')
     .attr('transform', `translate(0, ${height - margin.bottom})`)
     .call(d3.axisBottom(xScale));
 
-  d3.select('.histogram')
+  histogram
     .selectAll('rect')
     .data(bins)
     .join('rect')
@@ -72,9 +72,24 @@ const createHistogram = (earnings) => {
     .attr('height', bin => yScale(bin.x0) - yScale(bin.x1) - barsGap)
     .attr('fill', barsColor);
 
+  const curveFactory = d3.line()
+    .x(bin => xScale(bin.length))
+    .y(bin => yScale(bin.x0) - (yScale(bin.x0) - yScale(bin.x1)) / 2)
+    .curve(d3.curveCatmullRom);
+
+  console.log(bins);
+  const zeroPoint = { length: 0, x0: 0, x1: 0 };
+  const maxPoint = { length: 0, x0: bins.at(-1).x1, x1: bins.at(-1).x1 };
+
+  histogram
+    .append('path')
+    .attr('d', curveFactory([zeroPoint, ...bins, maxPoint]))
+    .attr('fill', 'none')
+    .attr('stroke', 'magenta')
+    .attr('stroke-width', 2);
 };
 
 // Create Split Violin Plot
 const createViolin = () => {
-  
+
 };
