1f5c16844ba3e1bbee2740c6902e6e221b5ccf69
LuisGilGB <luis.gilgb@gmail.com>
2023-09-25 23:58:53 +0200

    Started development of the vertical histogram with React

--- react-version/project-2/src/App.tsx
+++ react-version/project-2/src/App.tsx
@@ -1,57 +1,75 @@
 import './App.css';
+import VerticalHistogram from './components/VerticalHistogram.tsx';
+import { useEffect, useState } from 'react';
+import { csv as loadCsv } from 'd3-dsv';
 
-const App = () => (
-  <div className="container">
-    <h1>Exploring the Tennis pays in 2019</h1>
-    <div className="row">
-      <div className="col-8">
-        <p className="intro">
-          Vamos Rafa. Vamos Rafa, vamos Rafa, vamos Rafa. Don Rafael Nadal
-          Parera. Don Carlos Alcaraz Garfia. Ya sabemos qué es lo que tiene Rafa
-          que no cabe ni en la Giralda #VamosRafa
-        </p>
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadCsv('data/tennis-players-2019.csv').then((data: unknown[]) => {
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Exploring the Tennis pays in 2019</h1>
+      <div className="row">
+        <div className="col-8">
+          <p className="intro">
+            Vamos Rafa. Vamos Rafa, vamos Rafa, vamos Rafa. Don Rafael Nadal
+            Parera. Don Carlos Alcaraz Garfia. Ya sabemos qué es lo que tiene
+            Rafa que no cabe ni en la Giralda #VamosRafa
+          </p>
+        </div>
+      </div>
+      <div id="viz">
+        <VerticalHistogram
+          data={data}
+          numberMapper={d => d.earnings_USD_2019}
+        />
+      </div>
+      <div className="tooltip">
+        <div className="name"></div>
+        <div className="home"></div>
+        <div className="total-earnings-section">
+          Total prize money: <span className="total-earnings"></span>
+        </div>
+      </div>
+
+      <div className="source">
+        <div className="label">Sources:</div>
+        <ul>
+          <li>
+            <a href="https://www.wtatennis.com/rankings/singles">
+              WTA Tour - Singles Rankings
+            </a>{' '}
+            Top Women tennis players on 2019-12-28
+          </li>
+          <li>
+            <a href="https://www.atptour.com/en/rankings/singles?rankDate=2019-12-30&rankRange=0-100">
+              ATP Tour - Singles Rankings
+            </a>{' '}
+            Top Men tennis players on 2019-12-30
+          </li>
+          <li>
+            <a href="https://www.espn.com/">ESPN</a> Prize money of each tennis
+            player for the year 2019
+          </li>
+        </ul>
+      </div>
+
+      <div className="note">
+        Note: The layout of this visualization is inspired by the Observable
+        notebook{' '}
+        <a href="https://observablehq.com/@juliobguedes/hollywood-age-gaps">
+          Hollywood Age Gaps
+        </a>
+        , by <a href="https://juliobguedes.dev/">Júlio Guedes</a>
       </div>
     </div>
-    <div id="viz"></div>
-    <div className="tooltip">
-      <div className="name"></div>
-      <div className="home"></div>
-      <div className="total-earnings-section">
-        Total prize money: <span className="total-earnings"></span>
-      </div>
-    </div>
-
-    <div className="source">
-      <div className="label">Sources:</div>
-      <ul>
-        <li>
-          <a href="https://www.wtatennis.com/rankings/singles">
-            WTA Tour - Singles Rankings
-          </a>{' '}
-          Top Women tennis players on 2019-12-28
-        </li>
-        <li>
-          <a href="https://www.atptour.com/en/rankings/singles?rankDate=2019-12-30&rankRange=0-100">
-            ATP Tour - Singles Rankings
-          </a>{' '}
-          Top Men tennis players on 2019-12-30
-        </li>
-        <li>
-          <a href="https://www.espn.com/">ESPN</a> Prize money of each tennis
-          player for the year 2019
-        </li>
-      </ul>
-    </div>
-
-    <div className="note">
-      Note: The layout of this visualization is inspired by the Observable
-      notebook{' '}
-      <a href="https://observablehq.com/@juliobguedes/hollywood-age-gaps">
-        Hollywood Age Gaps
-      </a>
-      , by <a href="https://juliobguedes.dev/">Júlio Guedes</a>
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
---
+++ react-version/project-2/src/components/VerticalHistogram.tsx
@@ -0,0 +1,62 @@
+import { scaleLinear } from 'd3-scale';
+import { bin, max } from 'd3-array';
+
+interface VerticalHistogramProps<T> {
+  data: T[];
+  width?: number;
+  height?: number;
+  numberMapper: (d: T) => number;
+}
+
+const VerticalHistogram = <T,>({
+  data,
+  width = 500,
+  height = 500,
+  numberMapper,
+}: VerticalHistogramProps<T>) => {
+  const maxValue = max(data, numberMapper) || 0;
+
+  const binFactory = bin().domain([0, maxValue]).thresholds(10);
+  const bins = binFactory(data.map(numberMapper));
+
+  const xScale = scaleLinear()
+    .domain([0, max(bins.map(bin => bin.length)) || 0])
+    .range([0, width]);
+  const yScale = scaleLinear()
+    .domain([0, (bins.at(-1) || { x1: 0 }).x1 || 0])
+    .range([0, height]);
+
+  return (
+    <svg
+      className="vertical-histogram"
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+    >
+      <text x={width / 2} y={5} textAnchor="middle" className="title">
+        Earnings of the top tennis players in 2019 (USD)
+      </text>
+      {bins.map((bin, index) => (
+        <g key={index} transform={`translate(0, ${yScale(bin.x0 || 0)})`}>
+          <rect
+            x={0}
+            y={0}
+            width={xScale(bin.length)}
+            height={yScale(bin.x1 || 0) - yScale(bin.x0 || 0)}
+            className="bar"
+          />
+          <text
+            x={xScale(bin.length) + 5}
+            y={(yScale(bin.x1 || 0) - yScale(bin.x0 || 0)) / 2}
+            alignmentBaseline="central"
+            className="label"
+          >
+            {bin.length}
+          </text>
+        </g>
+      ))}
+    </svg>
+  );
+};
+
+export default VerticalHistogram;
