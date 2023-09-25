3a279904c0445578bc889d1c152e1c8ce2fe1d71
LuisGilGB <luis.gilgb@gmail.com>
2023-09-26 00:25:16 +0200

    Added some improvements

--- react-version/project-2/src/App.tsx
+++ react-version/project-2/src/App.tsx
@@ -27,7 +27,7 @@ const App = () => {
       <div id="viz">
         <VerticalHistogram
           data={data}
-          height={1000}
+          height={800}
           numberMapper={d => d.earnings_USD_2019}
         />
       </div>
--- react-version/project-2/src/components/VerticalHistogram.tsx
+++ react-version/project-2/src/components/VerticalHistogram.tsx
@@ -1,10 +1,16 @@
-import { scaleLinear } from 'd3-scale';
-import { bin, max } from 'd3-array';
+import * as d3 from 'd3';
 
 interface VerticalHistogramProps<T> {
   data: T[];
   width?: number;
   height?: number;
+  margins?: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  };
+  barsGap?: number;
   numberMapper: (d: T) => number;
 }
 
@@ -12,20 +18,31 @@ const VerticalHistogram = <T,>({
   data,
   width = 500,
   height = 500,
+  margins = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20,
+  },
+  barsGap = 1,
   numberMapper,
 }: VerticalHistogramProps<T>) => {
   console.log('data', data);
-  const maxValue = max(data, numberMapper) || 0;
+  const maxValue = d3.max(data, numberMapper) || 0;
 
-  const binFactory = bin().domain([0, maxValue]).thresholds(20);
+  const binFactory = d3.bin().domain([0, maxValue]).thresholds(20);
   const bins = binFactory(data.map(numberMapper));
 
-  const xScale = scaleLinear()
-    .domain([0, max(bins.map(bin => bin.length)) || 0])
-    .range([0, width]);
-  const yScale = scaleLinear()
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(bins.map(bin => bin.length)) || 0])
+    .range([margins.left, width - margins.right])
+    .nice();
+  const yScale = d3
+    .scaleLinear()
     .domain([0, (bins.at(-1) || { x1: 0 }).x1 || 0])
-    .range([0, height]);
+    .range([height - margins.bottom, margins.top])
+    .nice();
 
   return (
     <svg
@@ -34,21 +51,26 @@ const VerticalHistogram = <T,>({
       height={height}
       viewBox={`0 0 ${width} ${height}`}
     >
-      <text x={width / 2} y={5} textAnchor="middle" className="title">
+      <text
+        x={width / 2}
+        y={margins?.top / 2}
+        textAnchor="middle"
+        className="title"
+      >
         Earnings of the top tennis players in 2019 (USD)
       </text>
       {bins.map((bin, index) => (
         <g key={index} transform={`translate(0, ${yScale(bin.x0 || 0)})`}>
           <rect
-            x={0}
-            y={0}
-            width={xScale(bin.length)}
-            height={yScale(bin.x1 || 0) - yScale(bin.x0 || 0)}
+            x={margins.left}
+            y={yScale(bin.x1 || 0)}
+            width={xScale(bin.length) - margins.left}
+            height={yScale(bin.x0 || 0) - yScale(bin.x1 || 0) - barsGap}
             className="bar"
           />
           <text
             x={xScale(bin.length) + 5}
-            y={(yScale(bin.x1 || 0) - yScale(bin.x0 || 0)) / 2}
+            y={(yScale(bin.x0 || 0) + yScale(bin.x1 || 0)) / 2}
             alignmentBaseline="central"
             className="label"
           >
