cefe389ac411b1db35b01448150d8c6a3d454e5e
LuisGilGB <luis.gilgb@gmail.com>
2023-10-05 23:56:23 +0200

    Started development of an Area component with a first naive approach

---
+++ react-version/project-2/src/components/area/Area.tsx
@@ -0,0 +1,21 @@
+import * as d3 from 'd3';
+
+const Area = ({ bins, xScale, yScale, areaColor }) => {
+  const areaFactory = d3
+    .area()
+    .x0(() => xScale(0))
+    .x1(bin => xScale(bin.length))
+    .y(bin => yScale(bin.x0) - (yScale(bin.x0) - yScale(bin.x1)) / 2)
+    .curve(d3.curveCatmullRom);
+
+  return (
+    <path
+      d={areaFactory(bins)}
+      fill={areaColor}
+      stroke="none"
+      fillOpacity={0.75}
+    />
+  );
+};
+
+export default Area;
